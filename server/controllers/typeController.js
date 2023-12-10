5faf07047c9429610d309c6ee28ad7e665235821
maksimput <82806350+maksimput@users.noreply.github.com>
2023-12-10 20:27:25 +0500

    version 0.2

--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -1,13 +1,18 @@
-class typeController {
-    async create(req,res){
+const {Type} = require('../models/models')
+const ApiError = require('../error/ApiError');
 
+class TypeController {
+    async create(req, res) {
+        const {name} = req.body
+        const type = await Type.create({name})
+        return res.json(type)
     }
 
-    async getAll (req,res){
-        
+    async getAll(req, res) {
+        const types = await Type.findAll()
+        return res.json(types)
     }
-
 
 }
 
-module.exports = new typeController()
+module.exports = new TypeController()
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,6 +1,7 @@
 const Router = require('express')
 const router = new Router()
 const userController = require('../controllers/userController')
+const authMiddleware = require('../middleware/authMiddleware')
 
 router.post('/registration', userController.registration )
 router.post('/login', userController.login)
